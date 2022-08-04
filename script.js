8daba58454a7731a8aaf883a337c934df9172602
Alfredo Kalu Orie <105680142+xclusivfred@users.noreply.github.com>
2022-08-04 16:20:55 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+var countEl = document.getElementById("count-el");
+var saveEl = document.getElementById("save-el");
+
+let count = 0;
+
+function increment() {
+    count += 1;
+    countEl.textContent = count;
+}
+
+function decrement() {
+    count -= 1;
+    countEl.textContent = count;
+}
+
+
+
+function save() {
+    let countStr = count + " - ";
+    saveEl.textContent += countStr;
+    countEl.textContent = 0;
+    count = 0;
+}
